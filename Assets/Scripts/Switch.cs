006b5d2fba3398ea1e5322a94b5599f38f0f7b29
Jibago <wani3327@gmail.com>
2022-11-28 02:22:11 +0900

    switch try

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Switch : MonoBehaviour
+{
+    bool s;
+    public UnityEvent switchOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("sw");
+        s = !s;
+        if (s) switchOn.Invoke();
+    }
+}
